26b5c7c686d2c7be29a174ee73ddd48d01ce2a0b
이경호 <dlrudgh1303@gmail.com>
2021-07-13 02:56:59 +0900

    client 파일 index 추가

---
+++ client/src/pages/Main/index.js
@@ -0,0 +1,76 @@
+function Main () {
+    const history = useHistory();
+    const location = useLocation();
+    const [loading, testDate , error, fetchDate] = useApiCall(
+        `${process.env.REACT_APP_API_SERVER}/api/board`
+    );
+
+    const [visible, setVisible] = useState(false);
+
+    if (!testDate) { 
+        return<></>;
+    }
+
+    if(loading) {
+        return<> 로딩중 ...</>;
+    }
+
+    if(error) {
+        return <> 엘 : {error}</>;
+    }
+
+    const BoardComponents = testDate.map((boardData) => {
+        return ( 
+            <Board 
+            key={boardData._id}
+            title={boardData.title}
+            category={boardData.category}
+            time={boardData.time}
+            price={boardData.price}
+            user={boardDate.user}
+            imageLink = {boardData.imageLink}
+            setBoardData={( => {
+                history.push(`/board/${boardData._id}`);
+            })}
+            />
+        );
+    });
+
+    const id  = location.pathname.split('/')[2];
+    const selectedBoardData = testData.find((el) => {
+        return el._id === id;
+    });
+
+
+    return  ( 
+        <div>
+            <Route exact path='/'>
+                <div className = 'board-components-wrapper'>
+                    {BoardComponents}
+                </div>
+            </Route>
+            <Route path = {`/board/: id`}>
+                <Detail
+                    boardData = {selectedBoardData}
+                    setData={ () => {}}
+                    setVisible = {setVisible}
+                    fetchDate = {fetchDate}
+                    />
+            </Route>
+            <button
+                className="open-button"
+                onClick = {() => setVisible((state) => !state)}
+            ></button>
+            {visible ? (
+                <Write
+                boardData = {selectedBoardData}
+                setData={() => {}}
+                setVisible = {setVisible}
+                fetchDate = {fetchDate}
+                />
+            ) : null}
+        </div>
+    );
+}
+
+export default Main;
